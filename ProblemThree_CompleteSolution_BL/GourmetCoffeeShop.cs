8b5b86278922259efb9e07bd62e4ba910a838cd3
99003649 <78849753+99003649@users.noreply.github.com>
2021-02-25 17:25:23 +0530

    Update GourmetCoffeeShop.cs

--- ProblemThree_CompleteSolution_BL/GourmetCoffeeShop.cs
+++ ProblemThree_CompleteSolution_BL/GourmetCoffeeShop.cs
@@ -6,8 +6,26 @@ using System.Threading.Tasks;
 
 namespace ProblemThree_CompleteSolution_BL
 {
-    public class GourmetCoffeeShop : GourmetShop, ITaxCompute
+   public class GourmetCoffeeShop : GourmetShop, ITaxCompute
     {
-        
+
+        public int GId;
+        public string GName;
+        public double GQuantity;
+        public GourmetCoffeeShop(int GId,string GName, double GQuantity)
+        {
+            this.GId = GId;
+            this.GName = GName;
+            this.GQuantity = GQuantity;
+        }
+
+
+
+         List<GourmetCoffeeShop> coffeePriceDict = new List<GourmetCoffeeShop>();
+        public GourmetCoffeeShop()
+        {
+            coffeePriceDict.Add(new GourmetCoffeeShop(1, "Arabica Coffee Powder Pre-Packed Container", 4));
+            coffeePriceDict.Add(new GourmetCoffeeShop(2, "Arabica Coffee Powder Pre-Packed Container", 2));
+        }
     }
 }
