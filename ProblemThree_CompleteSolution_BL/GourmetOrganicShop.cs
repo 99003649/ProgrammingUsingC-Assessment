2faa562ffff9e0f1f8d79f199865894b9d2c0a93
99003649 <78849753+99003649@users.noreply.github.com>
2021-02-25 17:27:43 +0530

    Update GourmetOrganicShop.cs

--- ProblemThree_CompleteSolution_BL/GourmetOrganicShop.cs
+++ ProblemThree_CompleteSolution_BL/GourmetOrganicShop.cs
@@ -6,8 +6,26 @@ using System.Threading.Tasks;
 
 namespace ProblemThree_CompleteSolution_BL
 {
-    public class GourmetOrganicShop : GourmetShop,ITaxCompute
+    public class GourmetOrganicShop : GourmetShop
     {
-        
+        public int OId;
+        public string OName;
+        public int OQuantity;
+        public GourmetOrganicShop(int OId, string OName, int OQuantity)
+        {
+            this.OId = OId;
+            this.OName = OName;
+            this.OQuantity = OQuantity;
+        }
+
+
+
+         List<GourmetOrganicShop> organicPriceDict = new List<GourmetOrganicShop>();
+        public GourmetOrganicShop()
+        {
+            organicPriceDict.Add(new GourmetOrganicShop(1, "Non Polished Tur Dal", 2));
+            organicPriceDict.Add(new GourmetOrganicShop(2, "Polished Tur Dal", 4));
+        }
+
     }
 }
